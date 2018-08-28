cfe848792956d1f9e4bdbae94bb8051228950761
bahsim <bahsim@mail.ru>
2018-08-28 09:05:26 +0600

    i18n is added again

---
+++ frontend/user/i18n/en.js
@@ -0,0 +1,9 @@
+
+export const NAME = 'NAME';
+export const VALUE_1 = 'VALUE 1';
+export const VALUE_2 = 'VALUE 2';
+export const VALUE_3 = 'VALUE 3';
+export const VALUE_4 = 'VALUE 4';
+export const SAVE = 'SAVE';
+export const CLOSE = 'CLOSE';
+export const DELETE = 'DELETE';
---
+++ frontend/user/i18n/ru.js
@@ -0,0 +1,9 @@
+
+export const NAME = 'НАИМЕНОВАНИЕ';
+export const VALUE_1 = 'ЗНАЧЕНИЕ 1';
+export const VALUE_2 = 'ЗНАЧЕНИЕ 2';
+export const VALUE_3 = 'ЗНАЧЕНИЕ 3';
+export const VALUE_4 = 'ЗНАЧЕНИЕ 4';
+export const SAVE = 'СОХРАНИТЬ';
+export const CLOSE = 'ЗАКРЫТЬ';
+export const DELETE = 'УДАЛИТЬ';
